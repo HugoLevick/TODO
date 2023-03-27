2ba9d737249358ab5a264792ad2a850cc5d62918
HugoLevick <hlals12346@gmail.com>
2023-03-27 14:48:52 -0600

    Organizacion

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.use(express.static("public"));
+
+server.listen(3000, () => {
+  console.log("listening on 3000");
+});
