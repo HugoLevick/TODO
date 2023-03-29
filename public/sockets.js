e7c010e1e33d708dc48b0d8fe11fc4e5539a373b
Juan Diego Salazar <juadi19@gmail.com>
2023-03-29 00:39:42 -0600

    POST, PATCH terminados

--- public/sockets.js
+++ public/sockets.js
@@ -1,7 +1,6 @@
 const socket = io()
 socket.on('nueva-tarea', tarea => printTask(tarea))
 socket.on('actualizar-tarea', tarea => {
-    const a = document.getElementById("t"+tarea.id); 
-    console.log(a)
+    document.getElementById("t"+tarea.id)?.remove(); 
     printTask(tarea)
 })
